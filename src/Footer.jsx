36ab7c3b1ffbda47d458cc9bd3389e64e29dae4e
hexzoner <hexzon@gmail.com>
2024-07-22 17:29:42 +0200

    create post page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div className="text-center">Footer</div>;
+  return (
+    <div className="text-center bg-base-300 py-6 text-lg font-semibold">
+      Fullstack Blog group project by: <br /> Felipe, Michal, Serge. <br /> 2024
+    </div>
+  );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,3 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
-  return <div className="text-center">Navbar</div>;
+  return (
+    <div className="text-center py-6 bg-base-300">
+      <ul className="flex justify-between max-w-[700px] m-auto text-lg font-semibold">
+        <NavLink to={"/"} className={({ isActive }) => (isActive ? "underline underline-offset-2" : "")}>
+          <li>Home</li>
+        </NavLink>
+        <NavLink to={"create-post"} className={({ isActive }) => (isActive ? "underline underline-offset-2" : "")}>
+          <li>Create a Post</li>
+        </NavLink>
+      </ul>
+    </div>
+  );
 }
